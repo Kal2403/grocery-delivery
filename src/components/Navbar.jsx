da299be8ddf1645b1519cee9a264046803929f3e
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-27 23:16:35 +0200

    Add Cart icon with item count

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,6 +30,11 @@ const Navbar = () => {
                     <input className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
                     <input src={assets.search_icon} alt='search' className='w-4 h-4' />
                 </div>
+
+                <div onClick={() => navigate("/cart")} className="relative cursor-pointer">
+                    <img src={assets.nav_cart_icon} alt='cart' className='w-6 opacity-80' />
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
+                </div>
             </div>
         </nav>
     )
