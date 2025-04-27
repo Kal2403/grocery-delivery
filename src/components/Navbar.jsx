2a530ea78eed95f1760cbba70271e18e7081cc63
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-27 23:19:55 +0200

    Build responsive mobile menu based on open state

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -53,6 +53,31 @@ const Navbar = () => {
                 {/* Menu Icon SVG */}
                 <img src={assets.menu_icon} alt='menu' />
             </button>
+
+            {/* Mobile Menu */}
+            { open && (
+            <div className={`${open ? 'flex' : 'hidden'} absolute top-[60px] left-0 w-full bg-white shadow-md py-4 flex-col items-start gap-2 px-5 text-sm md:hidden`}>
+                <NavLink to="/" onClick={() => setOpen(false)}>Home</NavLink>
+                <NavLink to="/products" onClick={() => setOpen(false)}>All Product</NavLink>
+                {user &&
+                    <NavLink to="/products" onClick={() => setOpen(false)}>My Orders</NavLink>
+                }
+                <NavLink to="/" onClick={() => setOpen(false)}>Contact</NavLink>
+                {!user ? (
+                    <button onClick={() => {
+                                setOpen(false);
+                                setShowUserLogin(true);
+                            }} className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
+                        Login
+                    </button>
+                ) : (
+                    <button onClick={logout} className="cursor-pointer px-6 py-2 mt-2 bg-primary hover:bg-primary-dull transition text-white rounded-full text-sm">
+                        Logout
+                    </button>
+                )}
+
+            </div>
+            )}
         </nav>
     )
 }
