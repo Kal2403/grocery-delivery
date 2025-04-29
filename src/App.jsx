263af6a8c7387bbd45f21f83968e2a0a7ca7f879
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-29 23:45:13 +0200

    Add router integration with basic route to Home

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 const App = () => {
-  return (
-    <div>Hello World!</div>
-  )
+
+    return (
+        <div>
+            <div className={`${isSellerPath ? "" : 'px-6 md:px-16 lg:px-24 xl:px-32'}`}>
+                <Routes>
+                    <Route path='/' element={<Home />} />
+                </Routes>
+            </div>
+
+        </div>
+    )
 }
 
 export default App;
