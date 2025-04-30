4b40f9fe56abfc5645fbaa39983b11a2ff8bb7f1
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-30 23:29:11 +0200

    Add category navigation with scroll to top

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,7 +1,10 @@
 import React from 'react';
 import { categories } from '../assets/assets';
+import { useAppContext } from '../context/AppContext';
 
 const Categories = () => {
+
+    const { navigate } = useAppContext();
  
     return (
         <div className='mt-16'>
@@ -14,6 +17,10 @@ const Categories = () => {
                             key={index} 
                             className='group cursor-pointer py-5 px-3 gap-2 rounded-lg flex flex-col justify-center items-center' 
                             style={{backgroundColor: category.bgColor}} 
+                            onClick={() => {
+                                navigate(`/products/${category.path.toLowerCase()}`);
+                                scrollTo(0,0)
+                            }}
                         >
                             <img src={category.image} alt={category.text} 
                             className='group-hover:scale-108 transition max-w-28'/>
