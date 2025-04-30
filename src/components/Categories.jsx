62b02f2f7182193e3757b1acd23fd98fb857e3ca
Cristhian Castillo <kal2403199255@gmail.com>
2025-04-30 23:24:49 +0200

    Add title for categories section

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const Categories = () => {
-
+ 
     return (
         <div className='mt-16'>
+
+            <p className='text-2xl md:text-3xl font-medium'>Categories</p>
 
         </div>
     )
