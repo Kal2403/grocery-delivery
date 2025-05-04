8904a854a2f1d129bd3fe5137996c33bbcff639b
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-04 23:25:35 +0200

    Use context to get product list

--- src/components/BestSeller.jsx
+++ src/components/BestSeller.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
+import { useAppContext } from '../context/AppContext';
 
 const BestSeller = () => {
+
+    const { products } = useAppContext();
 
     return (
         <div className='mt-16'>
             <p className='text-2xl md:text-3xl font-medium'>Best Sellers</p>
             <div className='grid grid-cols-2 sm:grid-cols-3 md:grid-cols-3 gap-3 md:gap-6 lg:grid-cols-5 mt-6'>
-                
+
             </div>
         </div>
     )
