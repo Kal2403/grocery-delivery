f7744e881a01d2736c56f8ff7ff63076a24d437f
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-04 23:27:26 +0200

    Create base ProductCard component

---
+++ src/components/ProductCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ProductCard = ({ product }) => {
+
+    return (
+        <div className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white min-w-56 max-w-56 w-full">
+
+        </div>
+    );
+};
+
+export default ProductCard;
