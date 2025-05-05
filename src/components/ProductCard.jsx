15eea2c347253907ba6c5d89124cb459f7ff7846
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-05 23:53:35 +0200

    Load app context with cart and currency logic

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { assets } from '../assets/assets';
+import { useAppContext } from '../context/AppContext';
 
 const ProductCard = ({ product }) => {
+    const { currency, addToCart, updateCartItem, removeFromCart, cartItems, navigate } = useAppContext();
 
     return product && (
         <div className="border border-gray-500/20 rounded-md md:px-4 px-3 py-2 bg-white min-w-56 max-w-56 w-full">
