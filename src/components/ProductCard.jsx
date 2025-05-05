bec6e61ebf64b370771fcaeee8993f535fe7aea3
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-05 23:54:31 +0200

    Display price with offer and original price

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -19,6 +19,11 @@ const ProductCard = ({ product }) => {
                     ))}
                     <p>(4)</p>
                 </div>
+                <div className="flex items-end justify-between mt-3">
+                    <p className="md:text-xl text-base font-medium text-primary">
+                        {currency}${product.offerPrice}{" "} <span className="text-gray-500/60 md:text-sm text-xs line-through">{currency}${product.price}</span>
+                    </p>
+                </div>
             </div>
         </div>
     );
