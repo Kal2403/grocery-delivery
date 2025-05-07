9689224866a2868a4aefc63b4fbe2fa3b51b977c
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-08 00:56:43 +0200

    Create basic Footer component structure

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Footer = () => {
+
+    return (
+        <div className="px-6 md:px-16 lg:px-24 xl:px-32 mt-24 bg-primary/10">
+            
+        </div>
+    );
+};
+
+export default Footer;
