800e42950f3112ee1f1b2661a843f3caab68ec94
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-07 23:35:26 +0200

    Create basic NewsLetter component structure

---
+++ src/components/NewsLetter.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NewsLetter = () => {
+    
+    return (
+        <div className="flex flex-col items-center justify-center text-center space-y-2 mt-24 pb-14">
+            
+        </div>
+    )
+}
+
+export default NewsLetter;
