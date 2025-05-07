61f5d2aea7f15464953a98ed9bbb42dbd5f16275
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-07 23:27:40 +0200

    Add overlay container with heading

--- src/components/BottomBanner.jsx
+++ src/components/BottomBanner.jsx
@@ -6,6 +6,12 @@ const BottomBanner = () => {
     <div className='relative mt-24'>
         <img src={assets.bottom_banner_image} alt="banner" className='w-full hidden md:block' />
         <img src={assets.bottom_banner_image_sm} alt="banner" className='w-full md:hidden' />
+
+        <div className='absolute inset-0 flex flex-col items-center md:items-end md:justify-center pt-16 md:pt-0 md:pr-24'>
+            <div>
+                <h1 className='text-2xl md:text-3xl font-semibold text-primary mb-8 lg:text-6xl'>Why We Are The Best?</h1>
+            </div>
+        </div>
     </div>
   )
 }
