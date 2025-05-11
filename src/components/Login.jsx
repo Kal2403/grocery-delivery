d3984abc56844823f4469ae5392d410877bca53d
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-11 15:14:48 +0200

    Add form submit handler

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -10,9 +10,18 @@ const Login = () => {
     const [email, setEmail] = React.useState("");
     const [password, setPassword] = React.useState("");
 
+    const onSubmitHandler = async (event) => {
+        event.preventDefault();
+        setUser ({
+            email: "kal2403199277@gmail.com",
+            name: "KalTech"
+        })
+        setShowUserLogin(false)
+    }
+
     return (
         <div className='fixed top-0 bottom-0 left-0 right-0 z-30 flex items-center text-sm text-gray-600 bg-black/50'>
-            <form className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-80 sm:w-[352px] rounded-lg shadow-xl border border-gray-200 bg-white">
+            <form onSubmit={onSubmitHandler} className="flex flex-col gap-4 m-auto items-start p-8 py-12 w-80 sm:w-[352px] rounded-lg shadow-xl border border-gray-200 bg-white">
                 <p className="text-2xl font-medium m-auto">
                     <span className="text-primary">User</span> {state === "login" ? "Login" : "Sign Up"}
                 </p>
