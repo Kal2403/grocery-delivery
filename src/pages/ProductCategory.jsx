34bb275af538254c1cd8ff222ef8944f8fc068fa
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-13 23:08:14 +0200

    Add context and category filtering logic

--- src/pages/ProductCategory.jsx
+++ src/pages/ProductCategory.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import { useAppContext } from '../context/AppContext';
 import { useParams } from 'react-router-dom';
 
 const ProductCategory = () => {
 
+    const { products } = useAppContext();
     const { category } = useParams();
+
+    const filteredProducts = products.filter((product) => product.category.toLowerCase() === category)
 
     return (
         <div className='mt-16'>
