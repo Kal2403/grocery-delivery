e0a9ae23a6dfcd405d2109989513c110ce771979
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-13 23:05:49 +0200

    Add useParams to get category from URL

--- src/pages/ProductCategory.jsx
+++ src/pages/ProductCategory.jsx
@@ -1,6 +1,9 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const ProductCategory = () => {
+
+    const { category } = useParams();
 
     return (
         <div className='mt-16'>
