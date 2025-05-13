93d7a53468486753c0b59e24264609fee51c84f7
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-13 22:53:06 +0200

    Add setSearchQuery and getCartAmount to AppContext

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -16,6 +16,7 @@ export const AppContextProvider = ({ children }) => {
     const [products, setProducts] = useState([]);
     
     const [cartItems, setCartItems] = useState({});
+    const [searchQuery, setSearchQuery] = useState({});
 
     const fetchProducts = async () => {
         setProducts(dummyProducts);
@@ -70,6 +71,8 @@ export const AppContextProvider = ({ children }) => {
         updateCartItem,
         removeFromCart,
         cartItems,
+        searchQuery,
+        setSearchQuery,
     }
 
     return (
