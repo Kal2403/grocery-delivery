0c1f30eb863ef08af11d5ce80260c81ba3fe8468
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-18 23:36:00 +0200

    Create ProductDetails component with basic structure and data fetching

---
+++ src/pages/ProductDetails.jsx
@@ -0,0 +1,36 @@
+import React, { useEffect, useState } from 'react'
+import { useAppContext } from '../context/AppContext';
+import { Link, useParams } from 'react-router-dom';
+import { assets } from '../assets/assets';
+import ProductCard from '../components/ProductCard';
+
+const ProductDetails = () => {
+
+    const { products, navigate, currency, addToCart } = useAppContext();
+    const { id } = useParams();
+
+    const [relatedProducts, setRelatedProducts] = useState([]);
+    const [thumbnail, setThumbnail] = useState(null);
+
+    const product = products.find((item) => item._id === id);
+
+    useEffect(() => {
+        if (products.length > 0) {
+            let productsCopy = products.slice();
+            productsCopy = productsCopy.filter((item) => product.category === item.category)
+            setRelatedProducts(productsCopy.slice(0, 5))
+        }
+    }, [products])
+
+    useEffect(() => {
+        setThumbnail(product?.image[0] ? product.image[0] : null)
+    }, [product])
+
+    return product && (
+        <div className="mt-12">
+
+        </div>
+    );
+};
+
+export default ProductDetails;
