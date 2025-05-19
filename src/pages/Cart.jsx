a4fbf752024556ff29887b17218fc81bf9768e90
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-19 23:57:17 +0200

    Display individual cart items with quantity and subtotal

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -43,6 +43,37 @@ const Cart = () => {
                     <p className="text-center">Subtotal</p>
                     <p className="text-center">Action</p>
                 </div>
+
+                {cartArray.map((product, index) => (
+                    <div key={index} className="grid grid-cols-[2fr_1fr_1fr] text-gray-500 items-center text-sm md:text-base font-medium pt-3">
+                        <div className="flex items-center md:gap-6 gap-3">
+                            <div onClick={() => {
+                                navigate(`/products/${product.category.toLowerCase()}/${product._id}`),
+                                    scrollTo(0, 0)
+                            }} className="cursor-pointer w-24 h-24 flex items-center justify-center border border-gray-300 rounded">
+                                <img className="max-w-full h-full object-cover" src={product.image[0]} alt={product.name} />
+                            </div>
+                            <div>
+                                <p className="hidden md:block font-semibold">{product.name}</p>
+                                <div className="font-normal text-gray-500/70">
+                                    <p>Weight: <span>{product.weight || "N/A"}</span></p>
+                                    <div className='flex items-center'>
+                                        <p>Qty:</p>
+                                        <select onChange={e => updateCartitem(product._id, Number(e.target.value))} value={cartItems[product._id]} className='outline-none'>
+                                            {Array(cartItems[product._id] > 9 ? cartItems[product._id] : 9).fill('').map((_, index) => (
+                                                <option key={index} value={index + 1}>{index + 1}</option>
+                                            ))}
+                                        </select>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <p className="text-center">{currency}{product.offerPrice * product.quantity}</p>
+                        <button onClick={() => removeFromCart(product._id)} className="cursor-pointer mx-auto">
+                            <img src={assets.refresh_icon} alt="remove" className='inline-block w-6 h-6' />
+                        </button>
+                    </div>)
+                )}
             </div>
         </div>
     ) : null
