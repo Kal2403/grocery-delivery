f6ec3088971cacbccf6a81e564a281be87330312
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-19 23:59:08 +0200

    Add payment method selection

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -106,6 +106,13 @@ const Cart = () => {
                             </div>
                         )}
                     </div>
+
+                    <p className="text-sm font-medium uppercase mt-6">Payment Method</p>
+
+                    <select onChange={e => setPaymentOption(e.target.value)} className="w-full border border-gray-300 bg-white px-3 py-2 mt-2 outline-none">
+                        <option value="COD">Cash On Delivery</option>
+                        <option value="Online">Online Payment</option>
+                    </select>
                 </div>
             </div>
         </div>
