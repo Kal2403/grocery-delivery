ac63e4c10fe9557744bb168de5551380d19edc0b
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-19 23:58:44 +0200

    Implement order summary section with address selection

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -81,6 +81,33 @@ const Cart = () => {
                 </button>
 
             </div>
+
+            <div className="max-w-[360px] w-full bg-gray-100/40 p-5 max-md:mt-16 border border-gray-300/70">
+                <h2 className="text-xl md:text-xl font-medium">Order Summary</h2>
+                <hr className="border-gray-300 my-5" />
+
+                <div className="mb-6">
+                    <p className="text-sm font-medium uppercase">Delivery Address</p>
+                    <div className="relative flex justify-between items-start mt-2">
+                        <p className="text-gray-500">{selectedAddress ? `${selectedAddress.street},  ${selectedAddress.city}, ${selectedAddress.state}, ${selectedAddress.country}` : "No address found"}</p>
+                        <button onClick={() => setShowAddress(!showAddress)} className="text-primary hover:underline cursor-pointer">
+                            Change
+                        </button>
+                        {showAddress && (
+                            <div className="absolute top-12 py-1 bg-white border border-gray-300 text-sm w-full">
+                                { addresses.map((address, index) => (
+                                    <p onClick={() => {setSlectedAddresses(address); setShowAddress(false)}} className="text-gray-500 p-2 hover:bg-gray-100">
+                                        {address.street}, {address.city}, {address.state}, {address.country}
+                                    </p>
+                                ))}
+                                <p onClick={() => navigate("/add-address")} className="text-primary text-center cursor-pointer p-2 hover:bg-primary/10">
+                                    Add address
+                                </p>
+                            </div>
+                        )}
+                    </div>
+                </div>
+            </div>
         </div>
     ) : null
 }
