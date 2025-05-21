53ba29d2f24ab14ca1996c987830b17fab1c8ea2
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-21 23:31:24 +0200

    Update Navbar.jsx
    - Replace search icon input element with img ta
    - Make cart item count dynamic using getCartCount

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -34,12 +34,12 @@ const Navbar = () => {
 
                 <div className="hidden lg:flex items-center text-sm gap-2 border border-gray-300 px-3 rounded-full">
                     <input onChange={(e) => setSearchQuery(e.target.value)} className="py-1.5 w-full bg-transparent outline-none placeholder-gray-500" type="text" placeholder="Search products" />
-                    <input src={assets.search_icon} alt='search' className='w-4 h-4' />
+                    <img src={assets.search_icon} alt='search' className='w-4 h-4' />
                 </div>
 
                 <div onClick={() => navigate("/cart")} className="relative cursor-pointer">
                     <img src={assets.nav_cart_icon} alt='cart' className='w-6 opacity-80' />
-                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">3</button>
+                    <button className="absolute -top-2 -right-3 text-xs text-white bg-primary w-[18px] h-[18px] rounded-full">{getCartCount()}</button>
                 </div>
 
                 {!user ? (<button onClick={() => setShowUserLogin(true)} className="cursor-pointer px-8 py-2 bg-primary hover:bg-primary-dull transition text-white rounded-full">
