585d56a904f3d35594172ea350188e3e38a4830d
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-26 23:46:09 +0200

    Handle form submission and set seller login state

--- src/components/seller/SellerLogin.jsx
+++ src/components/seller/SellerLogin.jsx
@@ -1,12 +1,17 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
 const SellerLogin = () => {
 
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
+    const onSubmitHandler = async (event) => {
+        event.preventDefault();
+        setIsSeller(true);
+    }
+
     return !isSeller && (
-        <form className='min-h-screen flex items-center text-sm text-gray-600'>
+        <form onSubmit={onSubmitHandler} className='min-h-screen flex items-center text-sm text-gray-600'>
 
             <div className='flex flex-col gap-5 m-auto items-start p-8 py-12 min-w-80 sm:min-w-88 rounded-lg shadow-xl border border-gray-200'>
                 <p className='text-2xl font-medium m-auto'><span className='text-primary'>Seller</span> Login</p>
