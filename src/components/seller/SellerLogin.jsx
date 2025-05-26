cd03858b5f698750a1f6bc61a264a81ac23e01c7
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-26 23:32:17 +0200

    Create basic SellerLogin component structure

---
+++ src/components/seller/SellerLogin.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const SellerLogin = () => {
+
+    return (
+        <form className='min-h-screen flex items-center text-sm text-gray-600'>
+
+            <div className='flex flex-col gap-5 m-auto items-start p-8 py-12 min-w-80 sm:min-w-88 rounded-lg shadow-xl border border-gray-200'>
+                <p className='text-2xl font-medium m-auto'><span className='text-primary'>Seller</span> Login</p>
+            </div>
+
+        </form>
+    )
+}
+
+export default SellerLogin;
