6f5070107f91e518623e97412aead2c8fd04a705
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-27 23:24:40 +0200

    Add top header with logo and logout button

--- src/pages/seller/SellerLayout.jsx
+++ src/pages/seller/SellerLayout.jsx
@@ -1,10 +1,20 @@
 import React from 'react'
+import { assets } from '../../assets/assets';
+import { Link } from 'react-router-dom';
 
 const SellerLayout = () => {
 
     return (
         <>
-            
+            <div className="flex items-center justify-between px-4 md:px-8 border-b border-gray-300 py-3 bg-white">
+                <Link to='/'>
+                    <img className="cursor-pointer w-34 md:w-38" src={assets.logo} alt="logo" />
+                </Link>
+                <div className="flex items-center gap-5 text-gray-500">
+                    <p>Hi! Admin</p>
+                    <button ClassName='border rounded-full text-sm px-4 py-1'>Logout</button>
+                </div>
+            </div>
         </>
     );
 };
