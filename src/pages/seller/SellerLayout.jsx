76e8baecc2e24f510f62dfdd50ed8927998fd3c8
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-27 23:26:06 +0200

    Add sidebar layout container for seller dashboard

--- src/pages/seller/SellerLayout.jsx
+++ src/pages/seller/SellerLayout.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { assets } from '../../assets/assets';
-import { Link } from 'react-router-dom';
+import { Link, Outlet } from 'react-router-dom';
 
 const SellerLayout = () => {
 
@@ -12,8 +12,14 @@ const SellerLayout = () => {
                 </Link>
                 <div className="flex items-center gap-5 text-gray-500">
                     <p>Hi! Admin</p>
-                    <button ClassName='border rounded-full text-sm px-4 py-1'>Logout</button>
+                    <button onClick={logout} className='border rounded-full text-sm px-4 py-1'>Logout</button>
                 </div>
+            </div>
+            <div className='flex'>
+                <div className="md:w-64 w-16 border-r h-[550px] text-base border-gray-300 pt-4 flex flex-col transition-all duration-300">
+                    
+                </div>
+                <Outlet />
             </div>
         </>
     );
