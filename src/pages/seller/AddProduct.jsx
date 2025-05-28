c1d954fe0c04edb2eaa17aee749892de0968b5b7
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-28 23:44:05 +0200

    Create AddProduct component file with basic structure

---
+++ src/pages/seller/AddProduct.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const AddProduct = () => {
+
+    return (
+        <div className="no-scrollbar flex-1 h-[95vh] overflow-y-scroll flex flex-col justify-between">
+            <form className="md:p-10 p-4 space-y-5 max-w-lg">
+
+            </form>
+        </div>
+    );
+};
+
+export default AddProduct;
