62e7ce3e27faae2d13e50ff7687d4caa3c97e255
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-28 23:45:09 +0200

    Add useState hooks for product form data

--- src/pages/seller/AddProduct.jsx
+++ src/pages/seller/AddProduct.jsx
@@ -1,6 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AddProduct = () => {
+
+    const [files, setFiles] = useState([]);
+    const [name, setName] = useState('');
+    const [description, setDescription] = useState('');
+    const [category, setCategory] = useState('');
+    const [price, setPrice] = useState('');
+    const [offerPrice, setOfferPrice] = useState('');
 
     return (
         <div className="no-scrollbar flex-1 h-[95vh] overflow-y-scroll flex flex-col justify-between">
