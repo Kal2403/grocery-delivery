100a67481b7b9f7ff31c9891a2866c522b637e5b
Cristhian Castillo <kal2403199255@gmail.com>
2025-05-29 23:43:50 +0200

    Add table layout and headers to ProductList

--- src/pages/seller/ProductList.jsx
+++ src/pages/seller/ProductList.jsx
@@ -9,6 +9,21 @@ const ProductList = () => {
         <div className="no-scrollbar flex-1 h-[95vh] overflow-y-scroll flex flex-col justify-between">
             <div className="w-full md:p-10 p-4">
                 <h2 className="pb-4 text-lg font-medium">All Products</h2>
+                <div className="flex flex-col items-center max-w-4xl w-full overflow-hidden rounded-md bg-white border border-gray-500/20">
+                    <table className="md:table-auto table-fixed w-full overflow-hidden">
+                        <thead className="text-gray-900 text-sm text-left">
+                            <tr>
+                                <th className="px-4 py-3 font-semibold truncate">Product</th>
+                                <th className="px-4 py-3 font-semibold truncate">Category</th>
+                                <th className="px-4 py-3 font-semibold truncate hidden md:block">Selling Price</th>
+                                <th className="px-4 py-3 font-semibold truncate">In Stock</th>
+                            </tr>
+                        </thead>
+                        <tbody className="text-sm text-gray-500">
+
+                        </tbody>
+                    </table>
+                </div>
             </div>
         </div>
     );
